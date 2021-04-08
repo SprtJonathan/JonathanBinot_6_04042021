a608833296c61be54afff1ff796dc4e39d9d76c1
SprtJonathan <jonathan.binot@gmail.com>
2021-04-08 17:44:30 +0200

    Ajout du backend

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
+
+// Sécurisation des identifiants de connexion au cluster dans une variable d'environnement
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Connexion à la base
+mongoose
+  .connect(process.env.DB_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/api/sauce", sauceRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
